5a6f5f9bf8f03ed73cf7ac2150e3b563c92ec72b
api-scraping <bao.francis.nguyen@gmail.com>
2024-12-17 16:25:30 +0100

    delete viginere function

--- main.py
+++ main.py
@@ -43,19 +43,3 @@ def convert_password_to_list_of_keys(password):
 	for char in password:
 		list_of_keys.append(alphabet.index(char))
 	return list_of_keys
-
-
-
-def vigenere_cipher(message, password, reverse=False):
-	list_of_keys = convert_password_to_list_of_keys(password)
-	#list_of_keys = [alphabet.index(char) for char in password]
-	crypted_message = ""
-	for index, char in  enumerate(message):
-		current_key = list_of_keys[index % len(list_of_keys)]
-		
-		if reverse:
-			
-		crypted_message += cesar_uncipher(char, current_key)
-		else:
-            crypted_message += cesar_cipher(char, current_key)
-	return crypted_message
